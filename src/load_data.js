27574849169171655b9dffd5f08e0dcfc1c7e951
Julian Garcia <j.garciap.2024@alumnos.urjc.es>
2025-11-12 18:26:02 +0100

    agregar modelos de zapatillas a data.json y actualizar la vista de detalle para mostrar información dinámica

--- src/load_data.js
+++ src/load_data.js
@@ -1,5 +1,6 @@
 import fs from 'node:fs/promises';
 import * as board from './sneakersdb.js';
+import { ObjectId } from 'mongodb';
 
 const UPLOADS_FOLDER = './uploads';
 const DATA_FOLDER = './data';
@@ -12,8 +13,13 @@ const brands = JSON.parse(dataString);
 
 await board.deletePosts();
 for(let brand of brands){
+    for(let i = 0; i < brand.models.length; i++){
+        let model = brand.models[i];
+        model._id = new ObjectId();
+    }
     await board.addPost(brand);
 }
+
 
 await fs.rm(UPLOADS_FOLDER, { recursive: true, force: true });
 await fs.mkdir(UPLOADS_FOLDER);
